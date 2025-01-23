34d75f3f4aad5ee1ecf7999981441bcce4c1cc06
Mikhail Ivanov <aka.device@gmail.com>
2025-01-24 00:27:06 +0300

    - 2. Creating Car model, cars html file and list with 5 cars in CarController

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
+import java.util.List;
+import web.model.Car;
 
 @Controller
 @RequestMapping("/cars")
@@ -11,6 +14,14 @@ public class CarController {
 
     @GetMapping
     public String getCars(Model model) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Vaz", "Red", 100));
+        cars.add(new Car("Taz", "White", 200));
+        cars.add(new Car("Gaz", "Blue", 300));
+        cars.add(new Car("SyperTaz", "Black", 400));
+        cars.add(new Car("KOPbITO", "Invisible", 1000));
+
+        model.addAttribute("cars", cars);
         return "cars";
     }
 }
